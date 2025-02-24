c6f5e26eec645aacc4d0d0771f1310cb351c5e03
pspepp3r <prosperpepple12@gmail.com>
2025-02-24 21:36:27 +0100

    Formatted Application configuration

--- configs/app.php
+++ configs/app.php
@@ -2,10 +2,24 @@
 declare(strict_types=1);
 use Src\Enums\AppEnvironment;
 
+$boolean = function (mixed $value) {
+  if (in_array($value, ['true', 1, '1', true, 'yes'], true)) {
+    return true;
+  }
+
+  return false;
+};
+
 $appEnv = $_ENV['APP_ENV'] ?? AppEnvironment::Production->value;
-$formatAppName = strtolower(str_replace(' ', '_', $_ENV['APP_ENV'])); 
+$formatAppName = strtolower(str_replace(' ', '_', $_ENV['APP_NAME'])); 
 
 return [
+  'app' => [
+    'app_name' => AppEnvironment::isDevelopment($appEnv),
+    'app_version' => $_ENV['APP_VERSION'] ?? '1.0',
+    'app_environment' => $appEnv,
+    'app_debug' => (bool) ($_ENV['APP_DEBUG'] ?? 0)
+  ],
   'db' => [
     'app_env' => $_ENV['APP_ENV'],
     'dbname' => $_ENV['DB_DATABASE'],
@@ -14,12 +28,6 @@ return [
     'host' => $_ENV['DB_HOST'],
     'driver' => $_ENV['DB_DRIVER'] ?? 'pdo_mysql',
   ],
-  'app' => [
-    'app_name' => $_ENV['APP_NAME'],
-    'app_version' => $_ENV['APP_VERSION'] ?? '1.0',
-    'app_environment' => $appEnv,
-    'app_debug' => (bool) ($_ENV['APP_DEBUG'] ?? 0)
-  ],
   'error_handling' => [
     'log_errors' => true,
     'display_error_details' => true,
@@ -27,8 +35,9 @@ return [
   ],
   'session' => [
     'name' => "{$formatAppName}_session",
-    'httponly' => (bool) 1,
-    'secure' => (bool) 1,
-    'samesite' => 'lax'
-  ]
+    'flash_name' => "{$formatAppName}_flash",
+    'secure' => $boolean($_ENV['SESSION_SECURE'] ?? true),
+    'httponly' => $boolean($_ENV['SESSION_HTTP_ONLY'] ?? true),
+    'samesite' => $_ENV['SESSION_SAME_SITE'] ?? 'lax',
+  ],
 ];
